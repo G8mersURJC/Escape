983b555a041e99c98ecc8dfee83b106b9c59e58b
G8mersURJC <60510612+G8mersURJC@users.noreply.github.com>
2020-02-17 22:08:46 +0100

    Primera versión y build
    Primera versión y build,

---
+++ Project Escape/Assets/Scripts/CellMap.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellMap : MonoBehaviour
+{
+    Vector2 vSize;
+    private int[,] coordinates;
+    private Cell [,] cells;
+    public float posx;
+    public float posz;
+    public int iSizeX, iSizeY;
+    public GameObject[] goModelList;
+    private Vector2 vPlayerSpawn, vExit;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cells = new Cell[iSizeX, iSizeY];
+        /*
+            0 -> Empty cell
+            1 -> Cannot pass cell
+            2 -> Can destroy cell
+            3 -> Start
+            4 -> End
+        */
+        posx = 4.5f;
+        posz = 4.5f;
+        coordinates = new int[10, 10] {
+            {0, 0, 0, 2, 0, 0, 0, 1, 0, 4},
+            {0, 1, 1, 1, 0, 1, 0, 1, 2, 1},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 1, 0, 1, 1, 0, 0, 0},
+            {1, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 1, 1, 1, 1, 0, 2},
+            {0, 0, 0, 0, 1, 0, 1, 1, 0, 0},
+            {0, 3, 0, 0, 1, 0, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        /*
+        for (int i = 0; i < coordinates.GetLength(0); i++)
+        {
+            for(int j = 0; j < coordinates.GetLength(1); j++)
+            {
+                Vector3 position = new Vector3(j - posz, -0.5f, posx - i);
+                Quaternion rotation = new Quaternion();
+                cell.CellCode = coordinates[i, j];
+                Instantiate(cell, position, rotation);
+                if(cell.CellCode == 3)
+                {
+                    GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                    Instantiate(capsule);
+                    capsule.transform.Translate(0, 5, 0);
+                }
+            }
+        }*/
+
+        for (int i = 0; i < coordinates.GetLength(0); i++)
+        {
+            for (int j = 0; j < coordinates.GetLength(1); j++)
+            {
+                cells[i, j] = new Cell();
+                cells[i, j].SetCellCode(coordinates[i, j]);
+                cells[i, j].SetPos(new Vector2(j - posz, posx - i));
+               
+                if(coordinates[i,j] == 3)
+                {
+                    GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                    cells[i,j].SetModel(capsule);
+                    capsule.transform.Translate(0, 5, 0);
+                    vPlayerSpawn.x = j;
+                    vPlayerSpawn.y = i;
+                }
+                else
+                {
+                 
+                        cells[i, j].SetModel(goModelList[coordinates[i, j]]);
+                }
+                cells[i, j].SetBehaviour();
+
+            }
+        }
+        GameManager.manager.SpawnPlayer(vPlayerSpawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public int[,] getMap()
+    {
+        return coordinates;
+    }
+}
---
+++ Project Escape/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int posx = 0;
+    public int posz = 0;
+    public float speed;
+    public int currentDir;
+    public int newDir;
+    private CellMap controlador;
+    private float currentDistance; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        newDir = -1;
+        speed = 1.0f;
+        currentDir = -1;
+        currentDistance = 0;
+        controlador = GameObject.Find("CellContainer").GetComponent(typeof(CellMap)) as CellMap;
+        if (!controlador) Debug.Log("No encontrado");
+        ResetPos();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (newDir==-1) {
+            if (Input.GetKeyDown(KeyCode.W)) newDir = 0;
+            if (Input.GetKeyDown(KeyCode.A)) newDir = 1;
+            if (Input.GetKeyDown(KeyCode.S)) newDir = 2;
+            if (Input.GetKeyDown(KeyCode.D)) newDir = 3;
+            if (newDir>=0&&!canMoveTo(newDir)) newDir = -1;
+        }
+        else
+        {
+            rotateFacing(newDir);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            currentDistance += speed * Time.deltaTime;
+            if (currentDistance >= 1)
+            {
+                ResetPos();
+            }
+        }
+    }
+    private bool canMoveTo(int c)
+    {
+        int[,] mapa = controlador.getMap();
+        switch (c)
+        {
+            case 0://Arriba
+                if (posz == 0) return false;
+
+                if (mapa[posz - 1, posx] !=1 && mapa[posz - 1, posx] != 2)
+                {
+                    posz--;
+                    return true;
+                }
+                break;
+            case 1://Izquierda
+                if (posx == 0) return false;
+
+                if (mapa[posz, posx - 1] != 1 && mapa[posz, posx - 1] != 2)
+                {
+                    posx--;
+                    return true;
+                }
+                break;
+            case 2://Abajo
+                if (posz == 9) return false;
+
+                if (mapa[posz + 1, posx] != 1 && mapa[posz + 1, posx] != 2)
+                {
+                    posz++;
+                    return true;
+                }
+                break;
+
+            case 3://Derecha
+                if (posx == 9) return false;
+
+                if (mapa[posz, posx + 1] != 1 && mapa[posz, posx + 1] != 2)
+                {
+                    posx++;
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+    private bool rotateFacing(int newDir)
+    {
+        if (newDir != currentDir)
+        {
+            switch (newDir)
+            {
+                case 0:
+                    this.transform.rotation = Quaternion.Euler(0, 0, 0);
+                    break;
+                case 1:
+                    this.transform.rotation = Quaternion.Euler(0, -90, 0);
+                    break;
+                case 2:
+                    this.transform.rotation = Quaternion.Euler(0, 180, 0);
+                    break;
+                case 3:
+                    this.transform.rotation = Quaternion.Euler(0, 90, 0);
+                    break;
+            }
+            currentDir = newDir;
+        }
+        
+       
+
+        return false;
+    }
+
+    public void SetPos(Vector2 vPos)
+    {
+        posx = (int)vPos.x;
+        posz = (int)vPos.y;
+    }
+
+    void ResetPos()
+    {
+        //Centra el movimiendo a la casilla.
+        transform.position = new Vector3(posx - controlador.posz, 0.5f, controlador.posx - posz);
+        newDir = -1;
+        currentDistance = 0;
+        Debug.Log("Estoy en: " + posx + " " + posz);
+    }
+}
