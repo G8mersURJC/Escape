47be919a976cfb8dcba97789c8505e4d4d57eef5
jdrojo2018 <drojomartin@gmail.com>
2020-03-20 17:15:51 +0100

    Programación Interfaz: Corazones

---
+++ Project Escape/Assets/Scripts/Ingame HUD/HUDTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDTest : MonoBehaviour
+{
+    private LifeHUDController lifeController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeController = GameObject.Find("Main Camera").GetComponent<LifeHUDController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+
+            Debug.Log("PAF!");
+
+            //Recibir daño
+            lifeController.ModifyHealthByAmount(-1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("SANACIÓN!");
+
+            //Curación
+            lifeController.ModifyHealthByAmount(2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("CORAZÓN EXTRA!");
+            lifeController.ExpandMaxHealth();
+        }
+    }
+}
---
+++ Project Escape/Assets/Scripts/Ingame HUD/LifeHUDController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeHUDController : MonoBehaviour
+{
+    public GameObject goHearth, goHearthPanel;
+    public Sprite sFullHearth, sHalfHearth, sEmptyHearth;
+    public Vector3 v3HearthOffset;
+
+    private int iCurrentMaxHealth = 6;
+    private int iCurrentHealth;  //2 "toques" por corazón
+    private int iMaxHealth = 10; //10 "toques" con 5 corazones
+
+    public List<GameObject> lHearths;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iCurrentHealth = iCurrentMaxHealth;
+
+        lHearths = new List<GameObject>();
+
+        v3HearthOffset += new Vector3(20, -20, 0);
+
+        //Creamos tantos corazones como valor máximo actual tengamos
+        for (int i = 0; i < iCurrentMaxHealth; i += 2)
+        {
+            GameObject newHearth = Instantiate(goHearth, new Vector3(), Quaternion.identity);
+            newHearth.transform.SetParent(goHearthPanel.transform);
+            newHearth.transform.localPosition = v3HearthOffset;
+            lHearths.Add(newHearth);
+            v3HearthOffset += new Vector3(60, 0, 0);
+        }
+
+        //Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+        //Creamos tantos corazones como valor máximo actual tengamos
+    }
+
+    public void ExpandMaxHealth()
+    {
+        
+        if (iCurrentMaxHealth < iMaxHealth)
+        {
+            iCurrentMaxHealth += 2;
+
+            GameObject newHearth = Instantiate(goHearth, new Vector3(), Quaternion.identity);
+            newHearth.transform.SetParent(goHearthPanel.transform);
+            newHearth.transform.localPosition = v3HearthOffset;
+            lHearths.Add(newHearth);
+            v3HearthOffset += new Vector3(60, 0, 0);
+        }
+
+        iCurrentHealth = iCurrentMaxHealth;
+        UpdateSprites();
+    }
+
+    public void ModifyHealthByAmount(int amount)
+    {
+        iCurrentHealth += amount;
+
+        if (iCurrentHealth < 0)
+        {
+            iCurrentHealth = 0;
+        }
+        else if (iCurrentHealth > iCurrentMaxHealth)
+        { 
+            iCurrentHealth = iCurrentMaxHealth;
+        }
+
+        UpdateSprites();
+
+        Debug.Log("Me quedan "+iCurrentHealth+" de" +iCurrentMaxHealth);
+    } 
+
+    private void UpdateSprites()
+    {
+
+        int count = 2;
+        foreach(GameObject h in lHearths)
+        {
+            int dif = count - iCurrentHealth;
+
+            if (dif <= 0)
+            {
+                //Corazón lleno
+                h.GetComponent<Image>().sprite = sFullHearth;
+            }
+            else if(dif == 1)
+            {
+                //Corazón medio lleno
+                h.GetComponent<Image>().sprite = sHalfHearth;
+            }
+            else if(dif > 1)
+            {
+                //Corazón vacio
+                h.GetComponent<Image>().sprite = sEmptyHearth;
+            }
+
+            count += 2;
+        }
+    }
+
+    
+}
