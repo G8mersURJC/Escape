37711662486109857dd02850525f6a2663e2b252
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2020-03-20 19:45:56 +0100

    Added Enemy Logic
    Added basic enemy logic and some enemy prefabs

---
+++ Project Escape/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] goEnemies;
+    private CellMap controlador;
+    float timer;
+    public float fTimeBetweenSpawns = 1;
+
+    int posx, posz;
+
+    private List<GameObject> goEnemyList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        goEnemyList = new List<GameObject>();
+
+        controlador = GameObject.Find("CellContainer").GetComponent(typeof(CellMap)) as CellMap;
+        if (!controlador) Debug.Log("No encontrado");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateEnemies();
+        SpawnNewEnemy();
+    }
+
+    private void SpawnNewEnemy()
+    {
+        timer += Time.deltaTime;
+        if(timer > fTimeBetweenSpawns)
+        {
+            bool spawned = false;
+            do
+            {
+                posx = Random.Range(0, 10);
+                posz = Random.Range(0, 10);
+                if (controlador.getMap()[posz, posx] == 0) {
+                    Debug.Log("Spawned successfully " + "[" + posx + "," + posz + "]");
+                    spawned = true;
+                }
+                
+
+            } while (!spawned);
+
+            GameObject go = Instantiate(goEnemies[0], new Vector3(posx, 0, posz), Quaternion.identity);
+            go.transform.position = new Vector3(posx - controlador.posx, 2, controlador.posz - posz);
+            goEnemyList.Add(go);
+
+            timer = 0.0f;
+        }
+    }
+
+    private void UpdateEnemies()
+    {
+        /**
+        foreach (GameObject go in goEnemyList)
+        {
+            
+        }
+        //*/
+    }
+}
--- Project Escape/Assets/Scripts/PlayerController.cs
+++ Project Escape/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,7 +12,8 @@ public class PlayerController : MonoBehaviour
     public int currentDir;
     public int newDir;
     private CellMap controlador;
-    private float currentDistance; 
+    private float currentDistance;
+
     // Start is called before the first frame update
     void Start()
     {
