d3cdd83d239ae6d4c6dab3d7c1e95d419a9b7cc4
jdrojo2018 <drojomartin@gmail.com>
2020-03-21 00:03:52 +0100

    Interfaz: Ventana de controles
    En la escena "HUD Test" se han implementado las siguientes funcionalidades:
    - Ventana de Controles e introducir el nombre de usuario. Se cierra tras hacer clic en el botón de abajo. Se guarda el nombre introducido.
    El juego no continua hasta que no se cierra la ventana.
    
    - Control del medidor de salud: Implementados métodos:
    1. ModifyHealthByAmount(int amount).
     Para modificar el estado con un valor (negativo para daños y positivo para curarse)
    2. ExpandMaxHealth(). Para añadir un corazón de vida extra y curarse completamente.

--- Project Escape/Assets/Scripts/Ingame HUD/HUDTest.cs
+++ Project Escape/Assets/Scripts/Ingame HUD/HUDTest.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDTest : MonoBehaviour
-{
+{ 
+    public Button bStart;
+    public InputField ifName;
+    public GameObject goHowToPlayPanel;
+
     private LifeHUDController lifeController;
+
+    private bool bControlsWindowActive = true;
 
     // Start is called before the first frame update
     void Start()
@@ -12,30 +19,44 @@ public class HUDTest : MonoBehaviour
         lifeController = GameObject.Find("Main Camera").GetComponent<LifeHUDController>();
     }
 
+    public void StartButtonPressed()
+    {
+        //Guardar el nombre introducido...
+        string username = ifName.text;
+        Debug.Log("Mi nombre es "+username);
+
+        //Desactivamos la ventana
+        Destroy(goHowToPlayPanel);
+        bControlsWindowActive = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (!bControlsWindowActive)
         {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Debug.Log("PAF!");
 
-            Debug.Log("PAF!");
+                //Recibir daño
+                lifeController.ModifyHealthByAmount(-1);
+            }
 
-            //Recibir daño
-            lifeController.ModifyHealthByAmount(-1);
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                Debug.Log("SANACIÓN!");
+
+                //Curación
+                lifeController.ModifyHealthByAmount(2);
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("CORAZÓN EXTRA!");
+                lifeController.ExpandMaxHealth();
+            }
         }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Debug.Log("SANACIÓN!");
-
-            //Curación
-            lifeController.ModifyHealthByAmount(2);
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Debug.Log("CORAZÓN EXTRA!");
-            lifeController.ExpandMaxHealth();
-        }
+        
     }
 }
