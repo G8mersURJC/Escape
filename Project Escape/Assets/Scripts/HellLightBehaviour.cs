3b40ac4cc616d07630bc08ab2ae0001ecfa0fdaf
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2020-03-30 17:05:39 +0200

    Lights
    Tried some new lights, probably they will be added

---
+++ Project Escape/Assets/Scripts/HellLightBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellLightBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float fMinLight = 0.9f;
+    public float fMaxLight = 2.1f;
+    public float fChange;
+
+    Light lLight;
+    void Start()
+    {
+        lLight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fChange += 0.01f;
+        if (fChange > Mathf.PI) fChange = 0;
+        lLight.intensity = (1 - Mathf.Sin(fChange)) * fMinLight + Mathf.Sin(fChange) * fMaxLight;
+    }
+}
